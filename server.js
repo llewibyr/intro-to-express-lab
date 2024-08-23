a28d4a154aacaa4db82f182ad5bfa56831e8d43c
llewibyr <byronnlew@gmail.com>
2024-08-22 20:52:11 -0400

    Next commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// import express
+const express = require('express')
+
+// initialize the app
+const app = express()
+
+// define 
+app.get('/', ())
+
+
+// start the listener method
+app.listen(3000, () =>console.log(`The server is running`))
